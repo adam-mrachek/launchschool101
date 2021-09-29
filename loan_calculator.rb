b4960fb61913fe66ab1f68c1f008661ed5e4464b
adam <amrachek@outlook.com>
2021-09-29 13:54:04 -0600

    add input validations

--- loan_calculator.rb
+++ loan_calculator.rb
@@ -19,8 +19,9 @@ def number?(input)
   integer?(input) || float?(input)
 end
 
-def calculate_monthly_payment(amount, monthly_rate, duration_months)
-  amount * (monthly_rate / (1 - (1 + monthly_rate)**(-duration_months)))
+def calculate_monthly_payment(amount, apr, duration_months)
+  monthly_rate = (apr.to_f / 100) / 12                                             # Convert APR to percent then divde by 12 to get monthly rate
+  amount.to_f * (monthly_rate / (1 - (1 + monthly_rate)**(-duration_months.to_f)))
 end
 
 def prompt(string)
@@ -30,15 +31,35 @@ end
 prompt("Welcome to the Loan Calculator")
 prompt("Please enter the total loan amount:")
 
-loan_amount = gets.chomp.to_f
+loan_amount = ""
 
-prompt("Enter the Annual Percentage Rate (APR)-(if 5%, enter 5):")
-apr = gets.chomp.to_f / 100
-monthly_interest_rate = apr /12
+loop do
+  loan_amount = gets.chomp
+
+  if number?(loan_amount)
+    break
+  else
+    prompt("Please enter a valid loan amount.")
+  end
+end
+
+prompt("Enter the Annual Percentage Rate(APR), (ex. if 5%, enter 5):")
+
+apr = ""
+
+loop do
+  apr = gets.chomp
+
+  if number?(apr)
+    break
+  else
+    prompt("Please enter valid APR. Example: if APR is 5%, enter 5.")
+  end
+end
 
 prompt("Enter the duration of the loan in months:")
-duration_months = gets.chomp.to_f
+duration_months = gets.chomp
 
-monthly_payment = calculate_monthly_payment(loan_amount, monthly_interest_rate, duration_months)
+monthly_payment = calculate_monthly_payment(loan_amount, apr, duration_months)
 
 prompt("Your monthly payment will be: $#{monthly_payment.round(2)}")
