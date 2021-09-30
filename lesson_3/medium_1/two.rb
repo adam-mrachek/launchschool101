af360d2062f2d4d07ab66530cf930a0ee079c8b4
adam <amrachek@outlook.com>
2021-09-30 12:47:49 -0600

    start medium exercises

---
+++ lesson_3/medium_1/two.rb
@@ -0,0 +1,8 @@
+# The result of the following statement will be an error:
+# puts "the value of 40 + 2 is " + (40 + 2)
+
+puts "the value of 40 + 2 is #{(40 + 2)}"
+
+# OR
+
+puts "the value of 40 + 2 is " + (40 + 2).to_s
